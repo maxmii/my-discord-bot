e2c6d187e5cdcb45a52a192f1aae578f1743204d
maxamills <morrm046@gmail.com>
2022-08-12 09:51:38 +0100

    Add encouragement messages if user is depressed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-const { Discord, GatewayIntentBits } = require("discord.js")
-const fetch = require("node-fetch")
+const { Discord, GatewayIntentBits } = require('discord.js')
+const fetch = require('node-fetch')
 const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds] })
 const token = process.env.TOKEN
 
-client.on("ready", () => {
+const getQuote = () => {
+  return fetch('https://zenquotes.io/api/random')
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      return `${data[0].q} - ${data[0].q}`
+    })
+}
+
+const sadWords = ['sad', 'depressed', 'unhappy', 'angry', 'miserable']
+const encouragements = [
+  'Cheer up',
+  'Hang in there!',
+  'You are a great person / bot!',
+]
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("pong")
+client.on('message', (msg) => {
+  if (msg.author.bot) return
+
+  if (msg.content === '$inspire') {
+    getQuote().then((quote) => msg.channel.send(quote))
+  }
+
+  if (sadWords.some(word => msg.content.incudes(word.toLowerCase()))) {
+    const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)]
+    msg.reply(encouragement)
   }
 })
 
